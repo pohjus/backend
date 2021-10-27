517cb86e7daa001a49e6eedccb51f9742cd0be2f
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-10-27 12:28:16 +0300

    fronttis

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 // http://localhost:3000/
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
